e2b159a0212921d4c99512917831aeff3f755660
Emartinez1814 <aaa.29laga@gmail.com>
2024-09-05 20:02:05 -0500

    creacion de los archivos funciones_tienda y procesar compras

---
+++ PARCIAL_1/procesar_compras.php
@@ -0,0 +1,19 @@
+<?php
+include 'funciones_tienda.php';
+
+$titulos = [
+    'camisa' => 'Página de Inicio',
+    'pantalon' => 'Sobre Nosotros',
+    'zapato' => 'Contáctanos',
+    'calcetines' => 'Contáctanos',
+    'gorra' => 'Contáctanos'
+];
+
+
+/*$subtotal=1000;
+echo "la compra es de: $subtotal<br>";
+$descuento=calcular_descuento($subtotal);
+$impuesto=aplicar_impuesto($descuento);
+$compra1=calcular_total($subtotal, $descuento, $impuesto);
+*/
+?>
