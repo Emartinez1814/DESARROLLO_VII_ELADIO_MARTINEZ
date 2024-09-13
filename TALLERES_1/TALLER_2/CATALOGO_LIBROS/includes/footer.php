11103e3500261e9f6377b37482c9638f2dccca8a
Emartinez1814 <aaa.29laga@gmail.com>
2024-09-12 19:03:28 -0500

    Reestructuracion

--- TALLERES_1/TALLER_2/CATALOGO_LIBROS/includes/footer.php
+++
@@ -1,8 +0,0 @@
-</main>
-    <footer>
-    <p>------------------------------------------------------------------------------------------------------------</p>
-    <p>Este es el contenido específico de la página de Catalogos.</p>
-        <p>&copy; <?php echo date('Y'); ?> Mi Sitio Web. Todos los derechos reservados.</p>
-    </footer>
-</body>
-</html>
